9b6fa80c469b6a8a21db398a3bd2e074afe29c4c
pshekhter <pshekhter@gmail.com>
2017-01-24 08:51:59 -0500

    Version 1

---
+++ src/testinggit/Movie.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testinggit;
+
+/**
+ *
+ * @author pshekhter
+ */
+public class Movie {
+    
+    private String title;
+    private String author;
+    private int numberPages;
+
+    public Movie(String title, String author, int numberPages) {
+        this.title = title;
+        this.author = author;
+        this.numberPages = numberPages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumberPages() {
+        return numberPages;
+    }
+
+    public void setNumberPages(int numberPages) {
+        this.numberPages = numberPages;
+    }
+    
+    
+    
+}
